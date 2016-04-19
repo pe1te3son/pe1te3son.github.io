66d08e4d58e95bb0992ab5a6f819e8bcb3cda101
Peter Janak <2010janak2010@gmail.com>
2016-04-20 00:02:39 +0100

    Work section added

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-"use strict";function offcanvas(){var a=$("#main-navigation"),n=$("#shift-body"),f=$("#nav-switch");f.click(function(){a.toggleClass("offCanvas"),n.toggleClass("offCanvas")})}var $=jQuery;$(document).ready(function(){offcanvas()});
+"use strict";function offcanvas(){var a=$("#main-navigation"),s=$("#shift-body"),f=$("#nav-switch");f.click(function(){a.toggleClass("offCanvas"),s.toggleClass("offCanvas"),f.hasClass("fa-bars")?$(this).removeClass("fa-bars").addClass("fa-times"):$(this).removeClass("fa-times").addClass("fa-bars")})}$(document).ready(function(){offcanvas()});
