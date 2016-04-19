260b76cfd23350801084c514e2483916ab1a11af
Peter Janak <2010janak2010@gmail.com>
2016-04-19 03:18:03 +0100

    Deleted old files, Initial commit for new website

---
+++ scripts/main.js
@@ -0,0 +1,1 @@
+"use strict";function offcanvas(){var a=$("#main-navigation"),n=$("#shift-body"),f=$("#nav-switch");f.click(function(){a.toggleClass("offCanvas"),n.toggleClass("offCanvas")})}var $=jQuery;$(document).ready(function(){offcanvas()});
